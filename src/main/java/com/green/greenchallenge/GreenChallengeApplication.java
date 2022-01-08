be90ccf747e2b22873dddc1fb33a3e35058c9189
cfhj8520 <cfhj8520@naver.com>
2022-01-08 16:52:29 +0900

    Initial commit

---
+++ src/main/java/com/green/greenchallenge/GreenChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.green.greenchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/greenchallenge/GreenChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.greenchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
