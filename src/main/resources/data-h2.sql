2308a64b98adc85f4ff98530c36561acfd25e302
Hyunjun <cfhj8520@naver.com>
2022-01-12 20:46:21 +0900

    conf : update DB add user_id column

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,5 +1,5 @@
-INSERT INTO user(id, password, name, age)
-VALUES ('test1', 'test1', 'test1', 10),
-       ('test2', 'test2', 'test2', 15),
-       ('test3', 'test3', 'test3', 20)
+INSERT INTO user(user_id, id, password, name, age)
+VALUES (1, 'test1', 'test1', 'test1', 10),
+       (2, 'test2', 'test2', 'test2', 15),
+       (3, 'test3', 'test3', 'test3', 20)
 ;
--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -2,10 +2,11 @@ DROP TABLE IF EXISTS user CASCADE;
 
 CREATE TABLE user
 (
+    user_id      int,
     id          varchar(20),
     password    varchar(100),
     name        varchar(100),
     age         int,
 
-    PRIMARY KEY (id)
+    PRIMARY KEY (user_id)
 );
