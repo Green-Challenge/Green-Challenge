eb7d60c32108ce182f104e313d1c05bbde4669c7
cfhj8520 <cfhj8520@naver.com>
2022-01-13 18:06:36 +0900

    ref : remove needless code and change AUTO -> IDENTITY

--- src/main/java/com/green/greenchallenge/domain/User.java
+++ src/main/java/com/green/greenchallenge/domain/User.java
@@ -1,19 +1,14 @@
 package com.green.greenchallenge.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id", nullable = false)
     private int userId;
 
