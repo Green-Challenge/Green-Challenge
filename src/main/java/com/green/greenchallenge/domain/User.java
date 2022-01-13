1749886b274d7d78395354ca3dc5c592c9872702
cfhj8520 <cfhj8520@naver.com>
2022-01-13 17:03:15 +0900

    conf : @generatedvalue 오류 해결

--- src/main/java/com/green/greenchallenge/domain/User.java
+++ src/main/java/com/green/greenchallenge/domain/User.java
@@ -1,14 +1,19 @@
 package com.green.greenchallenge.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "user_id", nullable = false)
     private int userId;
 
