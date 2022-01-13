61d4c407021c4d4eadc7627cf3c03804e24cb622
cfhj8520 <cfhj8520@naver.com>
2022-01-13 18:03:22 +0900

    conf : setting springfox swagger

---
+++ src/main/java/com/green/greenchallenge/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.green.greenchallenge.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
+}
