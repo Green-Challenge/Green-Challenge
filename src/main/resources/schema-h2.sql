f5c346da3eee1ed68f6b5f45687e8b747af87c9e
isyoo1213 <moethns@gmail.com>
2022-01-17 19:51:44 +0900

    fix: update sql for resolve git coflictt

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -2,12 +2,12 @@ DROP TABLE IF EXISTS user CASCADE;
 
 CREATE TABLE user
 (
-    uid      LONG    auto_increment,
+    uid      bigint    auto_increment,
     email       varchar(100),
     password    varchar(100),
     name        varchar(100),
     nickname    varchar(100),
-    address     varchar(100),
+    address     varchar(100)
 
     PRIMARY KEY (uid)
 );
