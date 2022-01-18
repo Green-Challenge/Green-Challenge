4437c0e5e2591d998f2d56bd6e6aef24d7b64d9e
isyoo1213 <moethns@gmail.com>
2022-01-18 20:16:31 +0900

    feat: create Challenge Entity and Repository

---
+++ src/main/java/com/green/greenchallenge/domain/Challenge.java
@@ -0,0 +1,23 @@
+package com.green.greenchallenge.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+public class Challenge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "challenge_id", nullable = false)
+    private Long challengeId;
+
+    private String challengeName;
+    private String description;
+    private String challengeImg;
+    private String transportation;
+    private int goal;
+    private LocalDate startDate;
+    private LocalDate finishDate;
+}
