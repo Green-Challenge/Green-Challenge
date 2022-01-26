89437331b0e103f088e077e52870568ff3e50853
Celox16 <sck04184@naver.com>
2022-01-26 12:59:39 +0900

    TEMPORARY SAVE

---
+++ src/main/java/com/green/greenchallenge/security/provider/JWTAuthenticationProvider.java
@@ -0,0 +1,22 @@
+package com.green.greenchallenge.security.provider;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JWTAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
---
+++ src/main/java/com/green/greenchallenge/security/temp.java
@@ -0,0 +1,4 @@
+package com.green.greenchallenge.security;
+
+public class temp {
+}
