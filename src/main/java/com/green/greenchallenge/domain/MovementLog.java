7867bc45e7c2637e65447e95b6ddc153fc88059f
Celox16 <sck04184@naver.com>
2022-01-28 15:46:13 +0900

    fix : update MovementLog entity

--- src/main/java/com/green/greenchallenge/domain/MovementLog.java
+++ src/main/java/com/green/greenchallenge/domain/MovementLog.java
@@ -1,6 +1,7 @@
 package com.green.greenchallenge.domain;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -14,11 +15,15 @@ public class MovementLog {
     @ManyToOne
     @JoinColumn(name = "User")
     private User userId;
+
     @Column(nullable = false)
     private int distance;
+
     @Column(nullable = false)
     private String transportation;
+
     @Column(nullable = false)
+    @CreatedDate
     private LocalDate day;
 
 }
