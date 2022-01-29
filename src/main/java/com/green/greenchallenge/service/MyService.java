66d09381271fe17242af4b1ff267b7a4b07a3720
Celox16 <sck04184@naver.com>
2022-01-29 14:37:25 +0900

    feat : create profile func

---
+++ src/main/java/com/green/greenchallenge/service/MyService.java
@@ -0,0 +1,40 @@
+package com.green.greenchallenge.service;
+
+import com.green.greenchallenge.domain.User;
+import com.green.greenchallenge.dto.UserDTO;
+import com.green.greenchallenge.exception.CustomException;
+import com.green.greenchallenge.exception.ErrorCode;
+import com.green.greenchallenge.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MyService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserDTO createProfile(UserDTO userDTO) {
+        Optional<User> findUser = userRepository.findById(userDTO.getUserId());
+
+        if(findUser.isEmpty()) throw new CustomException(ErrorCode.USER_NOT_FOUND);
+
+        findUser.get().setProfileImg(userDTO.getProfileImg());
+        findUser.get().setNickName(userDTO.getNickName());
+        findUser.get().setSiNm(userDTO.getSiNm());
+        findUser.get().setSggNm(userDTO.getSggNm());
+
+        userRepository.save(findUser.get());
+
+        return UserDTO.builder()
+                .profileImg(findUser.get().getProfileImg())
+                .nickName(findUser.get().getNickName())
+                .siNm(findUser.get().getSiNm())
+                .sggNm(findUser.get().getSggNm())
+                .build();
+    }
+}
