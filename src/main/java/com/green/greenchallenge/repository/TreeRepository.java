c7513be1ce919af4afe4eed858108074b9dad403
isyoo1213 <moethns@gmail.com>
2022-02-03 19:46:23 +0900

    fix: fix some features on application.yml and repository

--- src/main/java/com/green/greenchallenge/repository/TreeRepository.java
+++ src/main/java/com/green/greenchallenge/repository/TreeRepository.java
@@ -2,12 +2,9 @@ package com.green.greenchallenge.repository;
 
 import com.green.greenchallenge.domain.Challenge;
 import com.green.greenchallenge.domain.Tree;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
-
 
 @Repository
 public interface TreeRepository extends JpaRepository<Tree, Long> {
