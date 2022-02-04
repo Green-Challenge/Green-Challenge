fce83b5effcb0b6a7fdb2f02fce84afe38d1cab3
Celox16 <sck04184@naver.com>
2022-02-04 20:08:29 +0900

    fix : change findByChallenge parameter and List

--- src/main/java/com/green/greenchallenge/repository/TreeInstanceRepository.java
+++ src/main/java/com/green/greenchallenge/repository/TreeInstanceRepository.java
@@ -5,9 +5,11 @@ import com.green.greenchallenge.domain.TreeInstance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TreeInstanceRepository extends JpaRepository<TreeInstance, Long> {
 
-    TreeInstance findByChallengeId(Long challengeId);
+    List<TreeInstance> findByChallengeId(Challenge challengeId);
 
 }
