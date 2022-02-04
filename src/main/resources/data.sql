afb1c95858baba975dbbc588661ce1b1dc8b9360
cfhj8520 <cfhj8520@naver.com>
2022-02-04 22:09:37 +0900

    remove : delete needless .sql file

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-#password:test
-insert into user(email, password, name, nick_name, profile_img, sgg_nm, si_nm, create_date)
-values("test1@naver.com", "$2a$10$ARSoDamyhnYprm10dH84SOq/Dr9hUW36grgjSqOku6Qse0k2rKzpC", "tester", "testtest", "test.jpg", "ssg", "si", now());
-insert into user(email, password, name, nick_name, profile_img, sgg_nm, si_nm, create_date)
-values("test2@naver.com", "$2a$10$ARSoDamyhnYprm10dH84SOq/Dr9hUW36grgjSqOku6Qse0k2rKzpC", "tester", "testtest", "test.jpg", "ssg", "si", now());
-insert into user(email, password, name, nick_name, profile_img, sgg_nm, si_nm, create_date)
-values("test3@naver.com", "$2a$10$ARSoDamyhnYprm10dH84SOq/Dr9hUW36grgjSqOku6Qse0k2rKzpC", "tester", "testtest", "test.jpg", "ssg", "si", now());
