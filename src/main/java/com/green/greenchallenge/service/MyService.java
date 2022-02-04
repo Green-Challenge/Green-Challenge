a1b5649de6c95bd7638c3559917b74632f31bcd4
Celox16 <sck04184@naver.com>
2022-02-04 20:09:17 +0900

    feat : 함께심은나무정보가져오기 func

--- src/main/java/com/green/greenchallenge/service/MyService.java
+++ src/main/java/com/green/greenchallenge/service/MyService.java
@@ -1,14 +1,17 @@
 package com.green.greenchallenge.service;
 
-import com.green.greenchallenge.domain.User;
+import com.green.greenchallenge.domain.*;
+import com.green.greenchallenge.dto.GetTreeTogetherDTO;
 import com.green.greenchallenge.dto.UserDTO;
 import com.green.greenchallenge.exception.CustomException;
 import com.green.greenchallenge.exception.ErrorCode;
-import com.green.greenchallenge.repository.UserRepository;
+import com.green.greenchallenge.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,12 +19,15 @@ import java.util.Optional;
 public class MyService {
 
     private final UserRepository userRepository;
+    private final ParticipantRepository participantRepository;
+    private final TreeInstanceRepository treeInstanceRepository;
+    private final TreeRepository treeRepository;
 
     @Transactional
     public UserDTO createProfile(UserDTO userDTO) {
         Optional<User> findUser = userRepository.findById(userDTO.getUserId());
 
-        if(findUser.isEmpty()) throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        if (findUser.isEmpty()) throw new CustomException(ErrorCode.USER_NOT_FOUND);
 
         findUser.get().setProfileImg(userDTO.getProfileImg());
         findUser.get().setNickName(userDTO.getNickName());
@@ -42,7 +48,7 @@ public class MyService {
     public UserDTO getProfile(Long userId) {
         Optional<User> findUser = userRepository.findById(userId);
 
-        if(findUser.isEmpty()) throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        if (findUser.isEmpty()) throw new CustomException(ErrorCode.USER_NOT_FOUND);
 
         return UserDTO.builder()
                 .sggNm(findUser.get().getProfileImg())
@@ -51,5 +57,46 @@ public class MyService {
                 .sggNm(findUser.get().getSggNm())
                 .token(findUser.get().getToken())
                 .build();
+    }
+
+    @Transactional
+    public ArrayList<GetTreeTogetherDTO> getTreeTogether(User user) {
+        List<Participant> participants = participantRepository.findByUserId(user);
+        ArrayList<GetTreeTogetherDTO> getTreeTogethers = new ArrayList<>();
+
+        if(participants.isEmpty()) {
+            throw new CustomException(ErrorCode.PARTICIPANT_EMPTY);
+        }
+
+        for (int i = 0; i < participants.size(); i++) {
+
+            int numberOfCompletions = 0;
+            List<TreeInstance> instances = treeInstanceRepository.findByChallengeId(participants.get(i).getChallengeId());
+            for(int j = 0; j < instances.size(); j++) {
+                if(instances.get(j).getFinishedDate() != null) {
+                    numberOfCompletions++;
+                }
+            }
+
+            if(treeRepository.findByChallengeId(participants.get(i).getChallengeId()).isEmpty()) {
+                throw new CustomException(ErrorCode.TREE_EMPTY);
+            }
+
+            Long treeId = treeRepository.findByChallengeId(participants.get(i).getChallengeId()).get(0).getTreeId();
+
+            GetTreeTogetherDTO togetherDTO = new GetTreeTogetherDTO(
+                    participants.get(i).getChallengeId().getChallengeId(),
+                    participants.get(i).getChallengeId().getChallengeName(),
+                    numberOfCompletions,
+                    treeId,
+                    (int)(participants.get(i).getTotalDistance() / participants.get(i).getChallengeId().getGoalDistance())
+            );
+
+            getTreeTogethers.add(togetherDTO);
+
+            numberOfCompletions = 0;
+        }
+
+        return getTreeTogethers;
     }
 }
