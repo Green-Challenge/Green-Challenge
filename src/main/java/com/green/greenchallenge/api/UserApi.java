bc93f3e52fc8615fdd5eb855001f8826986b746c
cfhj8520 <cfhj8520@naver.com>
2022-02-07 15:08:44 +0900

    remove : remove needless code

--- src/main/java/com/green/greenchallenge/api/UserApi.java
+++ src/main/java/com/green/greenchallenge/api/UserApi.java
@@ -1,6 +1,5 @@
 package com.green.greenchallenge.api;
 
-import com.green.greenchallenge.domain.User;
 import com.green.greenchallenge.dto.UserDTO;
 import com.green.greenchallenge.service.AuthService;
 import com.green.greenchallenge.service.UserService;
@@ -31,21 +30,6 @@ public class UserApi {
             return new ResponseEntity("사용가능한 이메일입니다.",HttpStatus.OK);
         else
             return new ResponseEntity("이미 사용중인 이메일입니다.", HttpStatus.CONFLICT);
-    }
-
-    @GetMapping("/profile/{userId}")
-    public ResponseEntity getProfile(@PathVariable long userId) {
-        return new ResponseEntity(userService.getProfile(userId), HttpStatus.OK);
-    }
-
-    @PutMapping("/profile")
-    public ResponseEntity updateProfile(@RequestBody UserDTO userDTO) {
-        return new ResponseEntity(userService.updateProfile(userDTO), HttpStatus.OK);
-    }
-
-    @PostMapping("/profile")
-    public ResponseEntity inputProfile(@RequestBody UserDTO userDTO) {
-        return new ResponseEntity(userService.updateProfile(userDTO), HttpStatus.OK);
     }
 
     @PostMapping("/auth/signin")
