991a8a44e67fd8c790ccdef58a304d2432e22a47
Celox16 <sck04184@naver.com>
2022-02-08 17:46:30 +0900

    feat : additional method findByTreeInstanceId

--- src/main/java/com/green/greenchallenge/repository/DonationLogRepository.java
+++ src/main/java/com/green/greenchallenge/repository/DonationLogRepository.java
@@ -2,6 +2,7 @@ package com.green.greenchallenge.repository;
 
 import com.green.greenchallenge.domain.DonationLog;
 import com.green.greenchallenge.domain.Participant;
+import com.green.greenchallenge.domain.TreeInstance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
@@ -9,4 +10,5 @@ import java.util.List;
 
 public interface DonationLogRepository extends JpaRepository<DonationLog, Long> {
     List<DonationLog> findByParticipantId(Participant participant);
+    List<DonationLog> findByTreeInstanceId(TreeInstance treeInstance);
 }
