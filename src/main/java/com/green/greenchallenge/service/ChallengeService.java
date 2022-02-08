3120cddd7b969fa6b1ea046c3672d24655529aae
Celox16 <sck04184@naver.com>
2022-02-08 17:47:23 +0900

    fix : add new tree instance, new donation list, pay reward token func

--- src/main/java/com/green/greenchallenge/service/ChallengeService.java
+++ src/main/java/com/green/greenchallenge/service/ChallengeService.java
@@ -146,20 +146,70 @@ public class ChallengeService {
 
     @Transactional
     public void addRecord(AddRecordDTO addRecordDTO) {
+        Optional<Challenge> challenge = challengeRepository.findById(addRecordDTO.getChallengeId());
+        Optional<User> user = userRepository.findById(addRecordDTO.getUserId());
+        Participant participant = participantRepository.findByUserIdAndChallengeId(user.get(), challenge.get());
+        double challengeGoalDistance = challenge.get().getGoalDistance();
+        int addLeaves =  ((int) ((participant.getTotalDistance() + addRecordDTO.getAchieved()) / challengeGoalDistance)) - ((int) (participant.getTotalDistance() / challengeGoalDistance));
 
-        Participant participant = participantRepository.findByUserIdAndChallengeId(
-                User.builder().userId(addRecordDTO.getUserId()).build(),
-                Challenge.builder().challengeId(addRecordDTO.getChallengeId()).build());
-
-        double challengeGoalDistance = challengeRepository.findById(addRecordDTO.getChallengeId()).get().getGoalDistance();
-
-        participant.setLeafCount( // 목표 이동거리를 넘겼을 경우 남은 나뭇잎 수를 증가시킴
-                participant.getLeafCount() +
-                        (
-                                ((int) ((participant.getTotalDistance() + addRecordDTO.getAchieved()) / challengeGoalDistance)) -
-                                        ((int) (participant.getTotalDistance() / challengeGoalDistance))
-                        )
+        participant.setLeafCount( // 목표 이동거리를 넘겼을 경우 나뭇잎 수를 증가시킴
+                participant.getLeafCount() + addLeaves
         );
+
+        for (; addLeaves > 0; addLeaves--) { // 만약 생성된 나뭇잎수가 1개 이상일 경우에 자동적으로 기부가 되도록 함
+            Optional<TreeInstance> treeInstance = treeInstanceRepository.findByChallengeIdAndFinishedDateIsNull(challenge.get());
+            if (treeInstance.isEmpty()) { // 만약 새로 시작할 인스턴스가 없을경우, 새로운 인스턴스를 생성한다.
+                treeInstanceRepository.save(
+                        TreeInstance.builder()
+                                .challengeId(challenge.get())
+                                .build()
+                );
+
+                Optional<TreeInstance> newTreeInstance = treeInstanceRepository.findByChallengeIdAndFinishedDateIsNull(challenge.get());
+                // 위에서 새로 생성시켰으므로, finished date가 null인 값이 존재한다. 해당 나무인스턴스의 기부로그를 추가한다.
+                newTreeInstance.get().setNumberOfLeaf(newTreeInstance.get().getNumberOfLeaf() + 1);
+                donationLogRepository.save(
+                        DonationLog.builder()
+                                .treeInstanceId(newTreeInstance.get())
+                                .participantId(participant)
+                                .donationDate(LocalDate.now())
+                                .build()
+                );
+
+            } else { // 시작할 인스턴스가 있는 경우 해당 인스턴스에 값을 저장한다.
+                treeInstance.get().setNumberOfLeaf(treeInstance.get().getNumberOfLeaf() + 1); // 나무인스턴스의 모인나뭇잎 수를 1 증가시킨다.
+                donationLogRepository.save( // 해당 나무인스턴스의 기부로그를 추가한다.
+                        DonationLog.builder()
+                                .treeInstanceId(treeInstance.get())
+                                .participantId(participant)
+                                .donationDate(LocalDate.now())
+                                .build()
+                );
+                if (treeInstance.get().getNumberOfLeaf() == challenge.get().getGoalLeaves()) {
+                    // 만약 모인 나뭇잎수가 챌린지의 목표 나뭇잎수에 도달할 경우 해당 인스턴스를 종료하고, 새로운 인스턴스를 생성한다.
+                    // 나무인스턴스가 완료되고 새로운 나무인스턴스가 생성된다.
+                    treeInstance.get().setFinishedDate(LocalDate.now());
+                    treeInstanceRepository.save(
+                            TreeInstance.builder()
+                                    .challengeId(challenge.get())
+                                    .build()
+                    );
+
+                    // 1. 나무인스턴스가 완료됐으므로, 해당 나무 인스턴스에 참여한 기부로그를 조회한다.
+                    List<DonationLog> donationLogList = donationLogRepository.findByTreeInstanceId(treeInstance.get());
+
+                    // 2. 해당 기부로그에서 참여번호로 해당유저를 찾은다음 리워드토큰을 지급한다.
+                    for (DonationLog donationLog : donationLogList) {
+                        Optional<User> donatedUser = userRepository.findById(
+                                participantRepository.findById(donationLog.getParticipantId().getParticipantId()).get().getUserId().getUserId()
+                        );
+                        donatedUser.get().setToken(
+                                donatedUser.get().getToken() + challenge.get().getRewardToken() / 100
+                        );
+                    }
+                }
+            }
+        }
 
         participant.setTotalDistance(participant.getTotalDistance() + addRecordDTO.getAchieved());
 
@@ -167,10 +217,10 @@ public class ChallengeService {
 
         movementLogRepository.save(
                 MovementLog.builder()
-                        .user(User.builder().userId(addRecordDTO.getUserId()).build())
+                        .user(user.get())
                         .distance(addRecordDTO.getAchieved())
                         .day(LocalDate.now())
-                        .transportation(challengeRepository.findById(addRecordDTO.getChallengeId()).get().getTransportation())
+                        .transportation(challenge.get().getTransportation())
                         .build()
         );
     }
