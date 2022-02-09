f98f78e556c470353472f2cd503bb32b8bf61c75
Celox16 <sck04184@naver.com>
2022-02-09 16:06:23 +0900

    conf : add ErrorCode

--- src/main/java/com/green/greenchallenge/exception/ErrorCode.java
+++ src/main/java/com/green/greenchallenge/exception/ErrorCode.java
@@ -17,7 +17,8 @@ public enum ErrorCode {
     PARTICIPANT_EMPTY(NOT_FOUND, "참여중인 challenge가 없습니다."),
     TREE_EMPTY(NOT_FOUND, "tree가 배정돼있지 않습니다."),
     LEAFCOUNT_ERROR(CONFLICT, "남아있는 나뭇잎을 불러올 수 없습니다."),
-    WRONG_PASSWORD(BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
+    WRONG_PASSWORD(BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    WRONG_VALUE(BAD_REQUEST, "올바르지 않은 값입니다.");
 
     private final HttpStatus httpStatus;
     private final String detail;
