1d574fac04a547fe5ac9560a96247e4ecde28a94
Celox16 <sck04184@naver.com>
2022-02-13 17:18:29 +0900

    feat : cors setting

--- src/main/java/com/green/greenchallenge/GreenChallengeApplication.java
+++ src/main/java/com/green/greenchallenge/GreenChallengeApplication.java
@@ -2,6 +2,9 @@ package com.green.greenchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GreenChallengeApplication {
@@ -10,4 +13,13 @@ public class GreenChallengeApplication {
         SpringApplication.run(GreenChallengeApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
